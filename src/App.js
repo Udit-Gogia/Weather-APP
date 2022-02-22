c6270208e2c1d6571e8ced827b54f728ae291ac9
Udit-Gogia <gogiaudit17@gmail.com>
2022-02-22 15:29:05 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Styles/App.scss"
+  
+const api = {
+  key : "dc2f797a7ece10a955d95046c5fa87c1",
+  base : "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="search-box">
+
+        <input type="text" placeholder="Enter City Name" className="search-bar"></input>
+      
+        </div> 
+        <div className="location-box">
+          <div className="location"></div>
+        </div> 
+        </div>
     </div>
   );
 }
